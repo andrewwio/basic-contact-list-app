c726f7ea6aa19766f4579eb47350f41e1ff9ecb8
Andrew Kain <hello@andreww.io>
2022-08-09 21:18:18 +0400

    finished

---
+++ src/components/EditableRow.js
@@ -0,0 +1,65 @@
+const EditableRow = ({ 
+  editFormData, 
+  handleEditFormChange,
+  handleCancelClick 
+}) => {
+  return (
+    <tr>
+      <td>
+        <input 
+          type="text"
+          name="fullName"
+          required="required"
+          placeholder="Enter a name..."
+          value={editFormData.fullName}
+          onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <input 
+          type="text"
+          name="address"
+          required="required"
+          placeholder="Enter an address..."
+          value={editFormData.address}
+          onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <input 
+          type="tel"
+          name="phoneNumber"
+          required="required"
+          placeholder="Enter a phone number..."
+          value={editFormData.phoneNumber}
+          onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <input 
+          type="email"
+          name="email"
+          required="required"
+          placeholder="Enter an email..."
+          value={editFormData.email}
+          onChange={handleEditFormChange}
+        />
+      </td>
+      <td>
+        <button 
+          type="submit"
+        >
+          Save
+        </button>
+        <button 
+          type="button"
+          onClick={handleCancelClick}
+        >
+          Cancel
+        </button>
+      </td>
+    </tr>
+  )
+}
+
+export default EditableRow
